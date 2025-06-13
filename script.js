51f1bdcd5576ff356efb67600f8f4af2ef55157f
narufitah <narufitah@gmail.com>
2025-06-13 12:58:37 +0200

    updates

--- script.js
+++ script.js
@@ -188,3 +188,30 @@ function logItems(arr) {
 }
 
 logItems(myCourses)
+
+// The generateSentence(desc, arr) takes two parameterer: a description and an array.
+// It should return a string based upon the description and array.
+
+// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
+// it should return the string: "The 3 largest countries are China, India, USA"
+
+// Example 2: If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
+// "The 2 best fruits are Apples, Bananas"
+
+// Use both a for loop and a template string to solve the challenge
+
+const fruits = ["Apples", "Bananas", "Oranges"]
+
+function generateSentence(desc, arr) {
+    // If desc = "best fruits" and arr.length = 3, then:
+    let sentence = `The ${arr.length} ${desc} are `
+    for (let i = 0; i < arr.length; i++) {
+    sentence += arr[i];
+    // Inside the loop, after each item except the last one, you want to add a comma and a space:
+    if (i < arr.length - 1) {
+    sentence += ", ";
+  }
+}
+    return sentence
+}
+console.log(generateSentence("best fruits", fruits))
